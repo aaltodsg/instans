f6d32ec282621982b2554a5844fa97ceb248d6dc
Esko Nuutila <Esko.Nuutila@aalto.fi>
2013-12-06 01:31:30 +0200

    Now you should be able to initialize the system by 'make initialize' in instans/src

--- src/check-configuration.sh
+++ src/check-configuration.sh
@@ -1,49 +1,75 @@
 #!/bin/sh
-sbcl --noinform --load output-configuration.lisp --eval '(output-configuration)' --quit
-
+LOG=log.config
+rm -f $LOG
+touch $LOG
+function msg () {
+    echo $* >> $LOG
+    echo $*
+}
+function tmp_to_log () {
+    cat $TMP >> $LOG
+    cat $TMP
+    rm $TMP
+}
+msg "Logging output to $LOG"
+TMP=$$_tmp
+sbcl --noinform --load output-configuration.lisp --eval "(with-open-file (str \"$TMP\" :direction :output :if-exists :supersede) (let ((*error-output* str) (*standard-output* str)) (output-configuration)))" --quit
+tmp_to_log
 CONFIG="LISP-CONFIGURATION"
-
 LISP_IMPLEMENTATION_TYPE=`sed -n "/^lisp-implementation-type:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
 LISP_IMPLEMENTATION_VERSION=`sed -n "/^lisp-implementation-version:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
 MINIMUM_SBCL_VERSION_FOR_INSTANS=`sed -n "/^minimum-sbcl-version-for-instans:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
 CONSISTENT_SBCL_VERSION=`sed -n "/^consistent-sbcl-version:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
 QUICKLISP=`sed -n "/^quicklisp:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
 ASDF=`sed -n "/^asdf:[ ]*\([^ ]*\)$/s//\1/p" ${CONFIG}`
-echo "Detected the following configuration:"
-echo "  LISP_IMPLEMENTATION_TYPE=$LISP_IMPLEMENTATION_TYPE"
-echo "  LISP_IMPLEMENTATION_VERSION=$LISP_IMPLEMENTATION_VERSION"
-echo "  MINIMUM_SBCL_VERSION_FOR_INSTANS=$MINIMUM_SBCL_VERSION_FOR_INSTANS"
-echo "  CONSISTENT_SBCL_VERSION=$CONSISTENT_SBCL_VERSION"
-echo "  QUICKLISP=$QUICKLISP"
-echo "  ASDF=$ASDF"
-echo
+msg
+msg "Detected the following configuration:"
+msg '  LISP_IMPLEMENTATION_TYPE=$LISP_IMPLEMENTATION_TYPE'
+msg '  LISP_IMPLEMENTATION_VERSION=$LISP_IMPLEMENTATION_VERSION'
+msg '  MINIMUM_SBCL_VERSION_FOR_INSTANS=$MINIMUM_SBCL_VERSION_FOR_INSTANS'
+msg '  CONSISTENT_SBCL_VERSION=$CONSISTENT_SBCL_VERSION'
+msg '  QUICKLISP=$QUICKLISP'
+msg '  ASDF=$ASDF'
+msg
 if test "$LISP_IMPLEMENTATION_TYPE" != "SBCL" -o $CONSISTENT_SBCL_VERSION = "no" ; then
-    echo "You seem to be running $LISP_IMPLEMENTATION_TYPE. Instans has only been tested with SBCL (minimum version $MINIMUM_SBCL_VERSION_FOR_INSTANS)."
-    echo "Please email esko.nuutila@aalto.fi if you are able to use it with $LISP_IMPLEMENTATION_TYPE."
+    msg "You seem to be running $LISP_IMPLEMENTATION_TYPE. Instans has only been tested with SBCL (minimum version $MINIMUM_SBCL_VERSION_FOR_INSTANS)."
+    exit 1
 fi
 if test "$CONSISTENT_SBCL_VERSION" = "no" ; then
-    echo "You seem to be running $LISP_IMPLEMENTATION_TYPE $LISP_IMPLEMENTATION_VERSION. Instans has only been tested version $LISP_IMPLEMENTATION_TYPE $MINIMUM_SBCL_VERSION_FOR_INSTANS."
+    msg "You seem to be running $LISP_IMPLEMENTATION_TYPE $LISP_IMPLEMENTATION_VERSION. Instans has only been tested version $LISP_IMPLEMENTATION_TYPE $MINIMUM_SBCL_VERSION_FOR_INSTANS."
+    exit 1
 fi
 if test "$QUICKLISP" = "no"; then
-    echo "No Quicklisp installed."
+    msg "No Quicklisp installed."
     /bin/echo -n "Would you like me to install it now (y/n)?"
     read answer
     if test "$answer" = "y" ; then
-      echo "Trying to download http://beta.quicklisp.org/quicklisp.lisp with curl"
+      msg "Trying to download http://beta.quicklisp.org/quicklisp.lisp with curl"
       curl -s -O http://beta.quicklisp.org/quicklisp.lisp
       if test $? -ne 0; then
-	  echo "Curl failed. Check http://www.quicklisp.org/beta/ how to install Quicklisp"
+	  msg "Curl failed. Check http://www.quicklisp.org/beta/ how to install Quicklisp"
+	  exit 1
       else
-	  echo "Trying to install Quicklisp into ~/quicklisp. Also updating your ~/.sbclrc file to automatically use Quicklisp"
-	  sbcl --noinform --load quicklisp.lisp --eval "(quicklisp-quickstart:install)" --eval "(ql:add-to-init-file)" --quit
-	  if test $? -ne 0; then
-	      echo "Some problem in installing quicklisp?"
-	  fi
+	  msg "Trying to install Quicklisp into ~/quicklisp. Also updating your ~/.sbclrc file to automatically use Quicklisp"
+	  sbcl --noinform --load quicklisp.lisp --eval "(with-open-file (str \"$TMP\" :direction :output :if-exists :supersede) (let ((*error-output* str) (*standard-output* str)) (quicklisp-quickstart:install)))" --eval  "(ql:add-to-init-file)" --quit
+	  tmp_to_log
       fi
+    else
+	exit 1
     fi
 fi
 if test "$ASDF" = "no"; then
-    echo "No ASDF found. It should come with $LISP_IMPLEMENTATION_TYPE. You may try to install it from http://common-lisp.net/project/asdf/ and then try again."
+    msg "No ASDF found. It should come with $LISP_IMPLEMENTATION_TYPE. You may try to install it from http://common-lisp.net/project/asdf/ and then try again."
+    exit 1
 fi
-
-
+msg
+msg "Creating a custom Lisp image for Instans in file 'sbcl.core-for-instans'."
+msg "After this, you should now be able to run sbcl with the special image by command:"
+msg
+msg "    sbcl --core sbcl.core-for-instans"
+msg
+msg "or by using make:"
+msg
+msg "make run-instans"
+msg
+exec sbcl --eval "(with-open-file (str \"$LOG\" :direction :output :if-exists :append) (let ((*error-output* str) (*standard-output* str)) (require 'sb-bsd-sockets) (require 'sb-posix) (require 'sb-introspect) (require 'sb-cltl2) (require 'asdf) (push (probe-file \".\") asdf:*central-registry*) (asdf:compile-system :instans) (asdf:load-system :instans) (in-package :instans) (save-lisp-and-die \"sbcl.core-for-instans\")))"
