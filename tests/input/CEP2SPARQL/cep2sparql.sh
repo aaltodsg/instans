e3ac88654a7fb3eda0398891acdc022b25757ff3
Mikko Rinne <mikko.rinne@aalto.fi>
2014-05-02 10:07:07 +0300

    Tested blank node generation with CEP2SPARQL (cep2sparqltest.sh in this update). Not working yet.

--- tests/input/CEP2SPARQL/cep2sparql.sh
+++ tests/input/CEP2SPARQL/cep2sparql.sh
@@ -1,1 +1,1 @@
-../../../bin/instans -r CEP2SPARQL_Queries_working.rq -g http://example.org/pattern/ -i CEP2SPARQL_SamplePattern.ttl -g http://example.org/source/ -i CEP2SPARQL_SampleEvents.ttl
+../../../bin/instans -r CEP2SPARQL_Queries_working.rq -g http://example.org/pattern/ -t CEP2SPARQL_SamplePattern.ttl -g http://example.org/source/ -t CEP2SPARQL_SampleEvents.ttl
