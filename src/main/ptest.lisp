3369f291188e411f862316acf661ab9a8ccd8f4a
Esko Nuutila <Esko.Nuutila@aalto.fi>
2014-07-03 21:51:16 +0300

    .

--- src/main/ptest.lisp
+++ src/main/ptest.lisp
@@ -5,8 +5,12 @@
 
 (in-package #:instans)
 
-(defun ptest ()
-  (let ((engine1 (instans-add-rules (parse-iri "http://instans.org/p1") "../tests/input/p1.rq" :encode-prefixes-p t))
-	(engine2 (instans-add-rules (parse-iri "http://instans.org/p2") "../tests/input/p2.rq" :encode-prefixes-p t)))
-    (
-    
+(defun ptest (&key base graph subscribe)
+  (let* ((input-iri 
+	  (engine1-iri (parse-iri "http://instans.org/p1"))
+	 (engine2-iri "http://instans.org/p2")
+	 (engine1 (instans-add-rules engine1-iri "../tests/input/p1.rq" :encode-prefixes-p t))
+	 (engine2 (instans-add-rules engine2-iri "../tests/input/p2.rq" :encode-prefixes-p t)))
+    (instans-add-query-input-processor engine1-iri 
+
+						     
