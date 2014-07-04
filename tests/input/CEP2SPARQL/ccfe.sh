c4cfd2b33e6b4783063aac5af029fcebbd4f19e8
Esko Nuutila <Esko.Nuutila@aalto.fi>
2014-07-04 17:45:04 +0300

    .

--- tests/input/CEP2SPARQL/ccfe.sh
+++ tests/input/CEP2SPARQL/ccfe.sh
@@ -1,1 +1,1 @@
-../../../bin/instans --report=statistics --allow-rule-instance-removal=false -r ccfe.rq --input=data/5actors10000events.ttl > data/i5sensors10000events.ttl
+../../../bin/instans --report=statistics --allow-rule-instance-removal=false -r ccfe.rq --input=data/5actors10000events.ttl --construct-output=ata/i5sensors10000events.ttl
