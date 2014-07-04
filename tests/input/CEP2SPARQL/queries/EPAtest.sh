0fdf846b6f55a36003ce1909cebdbfc55a5a88b5
Mikko Rinne <mikko.rinne@aalto.fi>
2014-07-04 15:47:18 +0300

    EPAtest to stdout.

--- tests/input/CEP2SPARQL/queries/EPAtest.sh
+++ tests/input/CEP2SPARQL/queries/EPAtest.sh
@@ -1,1 +1,1 @@
-../../../../bin/instans --construct-output=EPAtest.ttl -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/source --time=- --input-blocks=../data/i5sensors100events.ttl
+../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/source --time=- --input-blocks=../data/i5sensors100events.ttl
