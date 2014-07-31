e6b8f442832f01357165521a2b8749a09626c76a
Mikko Rinne <mikko.rinne@aalto.fi>
2014-07-31 16:49:54 +0300

    Added tests and isolated queries for cep2sparql EPA5-7.

---
+++ tests/input/cep2sparql-testing/queries/EPA7test.sh
@@ -0,0 +1,1 @@
+../../../../bin/instans -r EPA5.rq -r EPA7.rq -r select-printout.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/poststateful --time=- --input-blocks=../CEP2SPARQL_SampleEvents.ttl
