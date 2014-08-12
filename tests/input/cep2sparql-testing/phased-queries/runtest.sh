539ea5c566eb80a96e5165887592da41e5a6bafe
Esko Nuutila <Esko.Nuutila@aalto.fi>
2014-08-12 23:01:33 +0300

    .

--- tests/input/cep2sparql-testing/phased-queries/runtest.sh
+++ tests/input/cep2sparql-testing/phased-queries/runtest.sh
@@ -11,13 +11,18 @@ INPUT4=$PHOUTDIR/`basename $INPUT1 .ttl`-output3.trig
 INPUT5=$PHOUTDIR/`basename $INPUT1 .ttl`-output4.trig
 INPUT6=$PHOUTDIR/`basename $INPUT1 .ttl`-output5.trig
 
+echo $INSTANS --prefix-encoding=true --construct-output=$INPUT2 -r construct-EPA1.rq --rdf-operations=add:execute:flush -g http://instans.org/source --time=- --input-single=$INPUT1
 $INSTANS --prefix-encoding=true --construct-output=$INPUT2 -r construct-EPA1.rq --rdf-operations=add:execute:flush -g http://instans.org/source --time=- --input-single=$INPUT1
 
+echo $INSTANS --prefix-encoding=true --construct-output=$INPUT3 -r construct-EPA2.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT2
 $INSTANS --prefix-encoding=true --construct-output=$INPUT3 -r construct-EPA2.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT2
 
+echo $INSTANS --prefix-encoding=true --construct-output=$INPUT4 -r construct-EPA3.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT3
 $INSTANS --prefix-encoding=true --construct-output=$INPUT4 -r construct-EPA3.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT3
 
+echo $INSTANS --prefix-encoding=true --construct-output=$INPUT5 -r construct-EPA4.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT4
 $INSTANS --prefix-encoding=true --construct-output=$INPUT5 -r construct-EPA4.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT4
 
+echo $INSTANS --prefix-encoding=true --construct-output=$INPUT6 -r construct-EPA5.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT3
 $INSTANS --prefix-encoding=true --construct-output=$INPUT6 -r construct-EPA5.rq --rdf-operations=add:execute:remove:execute:flush --time=- --input-blocks=$INPUT3
 
