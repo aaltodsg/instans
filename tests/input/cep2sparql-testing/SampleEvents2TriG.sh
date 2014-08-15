a050f16d49ec617c000d048785763785ce3b0fb3
Mikko Rinne <mikko.rinne@aalto.fi>
2014-08-15 14:45:20 +0300

    Testing the use of TriG.

---
+++ tests/input/cep2sparql-testing/SampleEvents2TriG.sh
@@ -0,0 +1,1 @@
+instans --construct-output=CEP2SPARQL_SampleEvents.trig -r SampleEvents2TriG.rq --rdf-operations=add:execute-snapshot:remove:execute --input-blocks=CEP2SPARQL_SampleEvents.ttl
---
+++ tests/input/cep2sparql-testing/queries-trig/EPA1test.sh
@@ -0,0 +1,1 @@
+../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/source --time=- --input=../SCEP2SPARQL_SampleEvents.trig
