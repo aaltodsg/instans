7577609d77665e851c103b4ed8e1efb85ec734a3
Mikko Rinne <mikko.rinne@aalto.fi>
2014-08-15 17:29:39 +0300

    .

--- tests/input/cep2sparql-testing/SampleEvents2TriG.sh
+++ tests/input/cep2sparql-testing/SampleEvents2TriG.sh
@@ -1,1 +1,1 @@
-instans --construct-output=CEP2SPARQL_SampleEvents.trig -r SampleEvents2TriG.rq --rdf-operations=add:execute-snapshot:remove:execute --input-blocks=CEP2SPARQL_SampleEvents.ttl
+instans  --prefix-encoding=true --construct-output=CEP2SPARQL_SampleEvents.trig -r SampleEvents2TriG.rq --rdf-operations=add:execute-snapshot:remove:execute --input-blocks=CEP2SPARQL_SampleEvents.ttl
--- tests/input/cep2sparql-testing/queries-trig/EPA1test.sh
+++ tests/input/cep2sparql-testing/queries-trig/EPA1test.sh
@@ -1,2 +1,2 @@
 #../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute --time=- --input=../CEP2SPARQL_SampleEvents.trig
-../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute:remove:execute --time=- --input-blocks=../CEP2SPARQL_SampleEvents.trig
+../../../../bin/instans -r EPA1.rq --prefix-encoding=true --rdf-operations=add:execute:remove:execute --time=- --input-blocks=../CEP2SPARQL_SampleEvents.trig
