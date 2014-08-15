4adf6f95eb4401879972e7dc9083d487e1f954f0
Mikko Rinne <mikko.rinne@aalto.fi>
2014-08-15 14:48:11 +0300

    .

--- tests/input/cep2sparql-testing/queries-trig/EPA1test.sh
+++ tests/input/cep2sparql-testing/queries-trig/EPA1test.sh
@@ -1,1 +1,1 @@
-../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/source --time=- --input=../SCEP2SPARQL_SampleEvents.trig
+../../../../bin/instans -r EPA1.rq --rdf-operations=add:execute-snapshot:remove:execute -g http://instans.org/source --time=- --input=../CEP2SPARQL_SampleEvents.trig
