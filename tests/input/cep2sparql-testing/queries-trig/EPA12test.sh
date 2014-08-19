c6b38938ec2de3accb81cf72ed1cf423dc091aa1
Mikko Rinne <mikko.rinne@aalto.fi>
2014-08-19 18:21:59 +0300

    TriG-based EPA1-5 created, combination of EPA1+2 not working yet.

---
+++ tests/input/cep2sparql-testing/queries-trig/EPA12test.sh
@@ -0,0 +1,1 @@
+../../../../bin/instans --prefix-encoding=true -r EPA12.rq --rdf-operations=add:execute-snapshot:remove:execute:flush --time=- --input-blocks=../CEP2SPARQL_SampleEvents.trig
---
+++ tests/input/cep2sparql-testing/queries-trig/EPA4test.sh
@@ -0,0 +1,1 @@
+../../../../bin/instans --prefix-encoding=true -r EPA4.rq --rdf-operations=add:execute-snapshot:remove:execute:flush --time=- --input-blocks=../CEP2SPARQL_SampleEvents.trig
