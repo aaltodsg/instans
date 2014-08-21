a68d562f77d5fe6456df74c6d66a2861e590330e
Mikko Rinne <mikko.rinne@aalto.fi>
2014-08-21 16:20:06 +0300

    Cleaning up CEP2SPARQL TriG-cases.

--- tests/input/cep2sparql-testing/queries-trig/EPA7test.sh
+++ tests/input/cep2sparql-testing/queries-trig/EPA7test.sh
@@ -1,1 +1,1 @@
-../../../../bin/instans --prefix-encoding=true -r EPA7.rq --allow-rule-instance-removal=true --rdf-operations=add:execute-snapshot:flush --time=- --input-blocks=EPA5Output.trig
+../../../../bin/instans --prefix-encoding=true -r EPA7.rq --allow-rule-instance-removal=true --rdf-operations=add:execute-snapshot:flush --time=- --input-blocks=../data/EPA5_SampleEvents.trig
--- tests/input/cep2sparql-testing/queries-trig/EPAtest.sh
+++ tests/input/cep2sparql-testing/queries-trig/EPAtest.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-../../../../bin/instans --prefix-encoding=true -r $1 --allow-rule-instance-removal=true --rdf-operations=add:execute-snapshot:remove:execute:flush --time=- --input-blocks=../CEP2SPARQL_SampleEvents.trig
+../../../../bin/instans --prefix-encoding=true -r $1 --allow-rule-instance-removal=true --rdf-operations=add:execute-snapshot:remove:execute:flush --time=- --input-blocks=../data/CEP2SPARQL_SampleEvents.trig
