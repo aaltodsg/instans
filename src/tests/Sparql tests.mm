1bf20c16eed88ebc3c422b002495209fc127ae59
Esko Nuutila <Esko.Nuutila@aalto.fi>
2014-11-11 18:32:01 +0200

    .

---
+++ src/tests/Sparql tests.mm
@@ -0,0 +1,164 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1415709923381" ID="ID_1680653704" MODIFIED="1415709950253" TEXT="Sparql tests">
+<node CREATED="1415709950833" ID="ID_753070318" MODIFIED="1415709954245" POSITION="right" TEXT="Properties">
+<node CREATED="1415709954678" ID="ID_57463402" MODIFIED="1415709958965" TEXT="Suite"/>
+<node CREATED="1415709959331" ID="ID_439001924" MODIFIED="1415709961165" TEXT="Collection"/>
+<node CREATED="1415709961502" ID="ID_519304277" MODIFIED="1415709962501" TEXT="Name"/>
+<node CREATED="1415709974522" ID="ID_14376736" MODIFIED="1415710050471" TEXT="Rule type">
+<node CREATED="1415710050437" ID="ID_1197882811" MODIFIED="1415710054051" TEXT="Query">
+<node CREATED="1415709979394" ID="ID_743815219" MODIFIED="1415709984468" TEXT="Select test"/>
+<node CREATED="1415709984730" ID="ID_892391924" MODIFIED="1415709986980" TEXT="Ask test"/>
+<node CREATED="1415709987238" ID="ID_899687059" MODIFIED="1415709990996" TEXT="Describe test"/>
+<node CREATED="1415709991249" ID="ID_500340286" MODIFIED="1415709995284" TEXT="Construct test"/>
+</node>
+<node CREATED="1415710055321" ID="ID_1399291663" MODIFIED="1415710056906" TEXT="Update">
+<node CREATED="1415710085520" ID="ID_1930578053" MODIFIED="1415710099575">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Load
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710099578" ID="ID_1162034002" MODIFIED="1415710167738">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Clear
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710167742" ID="ID_1741629087" MODIFIED="1415710167745">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Drop
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710163029" ID="ID_410387174" MODIFIED="1415710163031">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Add
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710157565" ID="ID_55927871" MODIFIED="1415710157568">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Move
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710150997" ID="ID_1444424606" MODIFIED="1415710150999">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Copy
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710144086" ID="ID_1782834644" MODIFIED="1415710144089">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Create
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710139149" ID="ID_238841008" MODIFIED="1415710139151">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      InsertData
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710126982" ID="ID_1398565540" MODIFIED="1415710126985">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DeleteData
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710117886" ID="ID_850856691" MODIFIED="1415710117889">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      DeleteWhere
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node CREATED="1415710109471" ID="ID_738341669" MODIFIED="1415710109474">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Modify
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1415710188366" ID="ID_782448247" MODIFIED="1415710196963" TEXT="Query file"/>
+</node>
+</node>
+</map>
