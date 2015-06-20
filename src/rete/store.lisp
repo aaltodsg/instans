c33b1c0a2d4c3f9d0b9ae0e64bae531691a4a44b
Esko Nuutila <Esko.Nuutila@aalto.fi>
2015-06-21 01:25:39 +0300

    Fixed internal token handling for filter-memory and existence-start-node. Now they compare the correct part of token (see find-token-in-item). Conformance 586 success. At least part of Mikko's tests ok

--- src/rete/store.lisp
+++ src/rete/store.lisp
@@ -16,13 +16,26 @@
   (:method ((this filter-memory) token) (cddr token))
   (:method ((this memory) token) token))
 
+(defgeneric find-token-in-item (memory token item)
+  (:method ((this filter-memory) token item)
+    (declare (ignorable this))
+    (find token (cdr item) :test #'token-equal :key #'cdr))
+  (:method ((this existence-start-node) token item)
+    (declare (ignorable this))
+    (find token (cdr item) :test #'token-equal :key #'cddr))
+  (:method ((this memory) token item)
+    (declare (ignorable this))
+    (find token (cdr item) :test #'token-equal)))
+
 (defgeneric store-get-token (memory token)
   (:method ((this memory) token)
     (let ((key (second (car token))))
       (checkit (numberp key) "key ~S not a number" key)
       (let ((item (gethash key (memory-store this))))
+	;; (inform "~&item = ~S~%" item)
 	(and item
-	     (let ((stored-token (find token (cdr item) :test #'token-equal)))
+	     (let ((stored-token (find-token-in-item this token item)))
+	       ;; (inform "~&stored-token = ~S~%" stored-token)
 	       (and stored-token
 		    (let ((stored-token-inner (when-checkit (inner-token this stored-token))))
 		      (declare (ignorable stored-token-inner))
@@ -34,11 +47,14 @@
     (let ((key (second (car token))))
       (checkit (numberp key) "key ~S not a number" key)
       (let ((item (gethash key (memory-store this))))
+	;; (inform "~&item = ~S~%" item)
 	(cond ((null item)
 	       (setf (gethash key (memory-store this)) (list key token)))
 	      (t
-	       (checkit (not (find token (cdr item) :test #'token-equal)) "token ~S already in memory ~S" token this))
-	       (setf (cdr item) (cons token (cdr item)))))
+	       (let ((prev-token (find-token-in-item this token item)))
+		 ;; (inform "~&prev-token = ~S~%" prev-token)
+		 (checkit (not prev-token) "Memory ~S contains~%~S~%which is token-equal to~%~S" this prev-token token))
+	       (setf (cdr item) (cons token (cdr item))))))
       (incf (store-count this))
       (incf (memory-store-put-count this)))))
 
@@ -47,12 +63,14 @@
     (let ((key (second (car token))))
       (checkit (numberp key) "key ~S not a number" key)
       (let ((item (gethash key (memory-store this))))
+	;; (inform "~&item = ~S~%" item)
 	(checkit item "token ~S not in memory ~S" token this)
 	(loop for rest on item
 	      for other-token = (second rest)
 	      while other-token
 	      when (token-equal other-token token)
 	      do (progn
+		   ;; (inform "~&other-token = ~S~%" other-token)
 		   (cond ((= 2 (length item))
 			  (remhash key (memory-store this)))
 			 (t
